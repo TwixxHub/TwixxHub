cfb25037df050ab03af416a70c05e5977b32cb8c
TwixxHub <e.liprimmer@gmail.com>
2025-12-11 08:17:51 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,9 @@
+local url = "https://raw.githubusercontent.com/TwixxHub/TwixxHub/main/Main.lua"
+local source = game:HttpGet(url)
 
+local ok, result = pcall(loadstring(source))
+if not ok then
+    warn("[TwixxHub] Failed to load Main.lua:", result)
+end
+
+return result
